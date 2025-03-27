91be2c54ebb765f3d57bb96964baed8927ce30f2
Emily <emily@anaboth.com>
2025-03-27 20:00:06 -0300

    feat: simpler password verification

--- src/lib/server/password.ts
+++ src/lib/server/password.ts
@@ -1,6 +1,4 @@
 import { hash, verify } from '@node-rs/argon2';
-import { sha1 } from '@oslojs/crypto/sha1';
-import { encodeHexLowerCase } from '@oslojs/encoding';
 
 export async function hashPassword(password: string): Promise<string> {
 	return await hash(password, {
@@ -18,17 +16,6 @@ export async function verifyPasswordHash(hash: string, password: string): Promis
 export async function verifyPasswordStrength(password: string): Promise<boolean> {
 	if (password.length < 8 || password.length > 255) {
 		return false;
-	}
-	const hash = encodeHexLowerCase(sha1(new TextEncoder().encode(password)));
-	const hash_prefix = hash.slice(0, 5);
-	const response = await fetch(`https://api.pwnedpasswords.com/range/${hash_prefix}`);
-	const data = await response.text();
-	const items = data.split('\n');
-	for (const item of items) {
-		const hash_suffix = item.slice(0, 35).toLowerCase();
-		if (hash === hash_prefix + hash_suffix) {
-			return false;
-		}
 	}
 	return true;
 }
